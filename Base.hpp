96e7b5febb63e5d475cfe88c302cbf043be03fbe
Jonny <21113168+Jonny0201@users.noreply.github.com>
2018-05-01 15:33:18 +0800

    Delete the class named ListBase

--- Base.hpp
+++ Base.hpp
@@ -26,28 +26,6 @@ namespace DataStructure {
         virtual void free() = 0;
         virtual void allocate() = 0;
     };
-    template <typename T>
-    class ListBase {
-    public:
-        typedef T type;
-        typedef const T constType;
-        typedef T & reference;
-        typedef const T & constReference;
-        typedef T * pointer;
-        typedef const T * constPointer;
-        typedef const T *const constPointerConstant;
-        typedef T && valueReference;
-    private:
-        template <typename U>
-        struct Node {
-            U data;
-            Node *next {nullptr};
-        };
-    protected:
-        std::allocator<Node<T>> list;
-        Node<T> *first {nullptr};
-        virtual void free() = 0;
-    };
 }
 
 #endif //DATA_STRUCTURE_BASE_HPP
